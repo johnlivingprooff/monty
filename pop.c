87df74d3d121761c6b1a62be9b6ba6c185a7a65c
tgairwoman <tgjehovah@gmail.com>
2023-10-19 10:14:01 +0300

    monty

--- pop.c
+++ pop.c
@@ -7,16 +7,17 @@
  */
 void opcode_pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
+        stack_t *temp = *stack;
 
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+        if (*stack == NULL)
+        {
+                fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+                status = EXIT_FAILURE;  // Set the status variable to indicate the error
+                return;
+        }
 
-	*stack = (*stack)->next;
-	if (*stack != NULL)
-		(*stack)->prev = NULL;
-	free(temp);
+        *stack = (*stack)->next;
+        if (*stack != NULL)
+                (*stack)->prev = NULL;
+        free(temp);
 }
