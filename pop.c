08db776bd86b9e38b25c2e577c189026bb0205ac
johnlivingprooff <johnlivingprooff@gmail.com>
2023-10-19 07:12:16 +0200

    Updated pop.c

--- pop.c
+++ pop.c
@@ -9,13 +9,14 @@
  */
 void opcode_pop(stack_t **stack, unsigned int line_number)
 {
+    stack_t *temp = *stack;
+
     if (*stack == NULL)
     {
         fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
         exit(EXIT_FAILURE);
     }
 
-    stack_t *temp = *stack;
     *stack = (*stack)->next;
     if (*stack != NULL)
         (*stack)->prev = NULL;
