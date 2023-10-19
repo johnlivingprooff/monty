3481cf28caa06dc3392f5fd4829fefc3b17752a1
tgairwoman <tgjehovah@gmail.com>
2023-10-19 10:45:28 +0300

    monty

--- pop.c
+++ pop.c
@@ -1,23 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "monty.h"
 
 /**
- * opcode_pop - remove the top element of the stack
- * @stack: double pointer to the stack
- * @line_number: line number of the instruction
+ * pop - Remove the top element from the stack
+ * @stack: Double pointer to the stack
+ * @line_cnt: Line number of the instruction
+ *
+ * Return: Void
  */
-void opcode_pop(stack_t **stack, unsigned int line_number)
+void pop(stack_t **stack, unsigned int line_cnt)
 {
-        stack_t *temp = *stack;
+	stack_t *temp;
 
-        if (*stack == NULL)
-        {
-                fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-                status = EXIT_FAILURE;  // Set the status variable to indicate the error
-                return;
-        }
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%u: Can't pop an empty stack\n", line_cnt);
+		/* Handle the error here */
+		return;
+	}
 
-        *stack = (*stack)->next;
-        if (*stack != NULL)
-                (*stack)->prev = NULL;
-        free(temp);
+	temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+
+	free(temp);
 }
